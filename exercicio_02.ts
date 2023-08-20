ff763aa769e983d23b03e6ec20e3bbd1e58ab933
Kleberson <klebisvillela@outlook.com>
2023-08-19 23:27:45 -0300

    atividadae 2

---
+++ exercicio_02.ts
@@ -0,0 +1,5 @@
+const nome:string = 'Ely'
+const time:number = 120.56
+const language:string = 'TypeScript'
+
+console.log(`${nome}\nMy payment time is ${time}\nand\nmy preffered language is ${language}`)
